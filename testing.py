b3a7d12f0d0f6f4568a76983bc6db93dd1f4c09e
Zafiirah13 <zafiirah.hosenie@gmail.com>
2026-03-21 11:06:58 +0000

    make many chnages

--- testing.py
+++ testing.py
@@ -1,17 +1,26 @@
 """This is a test file for testing purposes."""
-
-# Function to add two numbers
-def add(c, a, b):
-    return c + a + b
-
-def multiply(a, b):
-    print("Multiplying numbers")
-    return a * b
-
 class Operation:
+    def __init__(self, c, d):
+        self.c = c
+        self.d = d
+        self.result = None
+
+    def divide(self):
+        if self.d != 0:
+            self.result = self.c / self.d
+        else:
+            return "Division by zero is not allowed."
+
+    def multiply(self):
+        return self.c * self.d
+
+Class LinearEquation:
     def __init__(self, a, b):
         self.a = a
         self.b = b
 
-    def subtract(self):
-        return self.a - self.b
+    def solve(self):
+        if self.a != 0:
+            return -self.b / self.a
+        else:
+            return "Coefficient a cannot be zero for a linear equation."

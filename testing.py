2d159bb7c21395baf07700b628dc0546d444ccd8
Zafiirah13 <zafiirah.hosenie@gmail.com>
2026-03-21 10:46:20 +0000

    add div

--- testing.py
+++ testing.py
@@ -4,8 +4,13 @@
 def add(c, a, b):
     return c + a + b
 
+def multiply(a, b):
+    print("Multiplying numbers...")
+    return a * b
+
 
 # Arry adding a new function
 def subtract(a, b, c, d):
     print("Subtracting numbers...")
+    div = a - b - c
     return c + d
